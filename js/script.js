0fffe64b3356de8ad782543c47bb6443dc9c7583
Aimar Fernandez <aimar.fernandez1@stu.ocsb.ca>
2025-03-31 17:07:23 +0000

    added js code

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
 // Created by: Aimar Fernandez
-// Created on: Feb 2025
+// Created on: Mar 2025
 // This file contains the JS functions for index.html
 
 function myButtonClicked() {
   // input
-  const base = parseFloat(document.getElementById("base").value)
+  const length = parseFloat(document.getElementById("length").value)
+  const width  = parseFloat(document.getElementById("width").value)
   const height = parseFloat(document.getElementById("height").value)
   // output
   document.getElementById("answer").innerHTML =
-    "Area is: " + base*height/2 + "cm²"
+    "Volume is: " + length*width*height/3 + "cm³"
 }
